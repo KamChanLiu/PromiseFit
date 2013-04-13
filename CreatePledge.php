b6a4a959c711cf0dcff70478853646950582b79c
Kam Liu <KamChanLiu@Gmail.com>
2013-04-13 20:23:20 +0800

    Added additional actions (create/read pledge, get charities)

---
+++ CreatePledge.php
@@ -0,0 +1,15 @@
+<?php
+	$conn = mysqli_connect("localhost", "root", "", "promisefit");
+	
+	$userid = $_POST["userid"];
+	$amount = $_POST["amount"];
+	$goodcharityid = $_POST["goodcharityid"];
+	$badcharityid = $_POST["badcharityid"];
+
+	$sql = "INSERT INTO pledges (UserId, Amount, GoodCharityId, BadCharityId, Date, Complete) VALUES ($userid, $amount, $goodcharityid, $badcharityid, NOW(), 0)";
+
+	echo $sql;
+
+
+	mysqli_query($conn, $sql) or die(mysqli_error($conn));;
+?>
